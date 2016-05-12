959a4744b82503fe111418cce63aedefde16b65e
Robbe De Geyndt <robbe.degeyndt@gmail.com>
2016-05-12 17:32:55 +0200

    Started work on OSM.

---
+++ Project 2/Code/Backend/OSMreader.cs
@@ -0,0 +1,20 @@
+﻿using OsmSharp.Osm.Xml.Streams;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class OSMreader
+    {
+        private FileInfo file { get; set; }
+        private XmlOsmStreamSource xmlSource { get; set; }
+        public OSMreader()
+        {
+
+        }
+    }
+}
